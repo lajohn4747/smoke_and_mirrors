bb09768f8a8589d101950200a3aaa0670a08e861
John P La <johnla@mit.edu>
2016-12-07 21:57:35 -0500

    Got cloth color to work

---
+++ linuxBuild/CMakeFiles/simulation.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/3rd_party/glew/src/glew.c" "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/linuxBuild/CMakeFiles/simulation.dir/3rd_party/glew/src/glew.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/src/camera.cpp" "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/linuxBuild/CMakeFiles/simulation.dir/src/camera.cpp.o"
+  "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/src/clothsystem.cpp" "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/linuxBuild/CMakeFiles/simulation.dir/src/clothsystem.cpp.o"
+  "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/src/main.cpp" "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/linuxBuild/CMakeFiles/simulation.dir/src/main.cpp.o"
+  "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/src/mirror.cpp" "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/linuxBuild/CMakeFiles/simulation.dir/src/mirror.cpp.o"
+  "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/src/particlesystem.cpp" "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/linuxBuild/CMakeFiles/simulation.dir/src/particlesystem.cpp.o"
+  "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/src/pendulumsystem.cpp" "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/linuxBuild/CMakeFiles/simulation.dir/src/pendulumsystem.cpp.o"
+  "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/src/rigidBall.cpp" "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/linuxBuild/CMakeFiles/simulation.dir/src/rigidBall.cpp.o"
+  "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/src/starter3_util.cpp" "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/linuxBuild/CMakeFiles/simulation.dir/src/starter3_util.cpp.o"
+  "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/src/timestepper.cpp" "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/linuxBuild/CMakeFiles/simulation.dir/src/timestepper.cpp.o"
+  "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/src/vertexrecorder.cpp" "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/linuxBuild/CMakeFiles/simulation.dir/src/vertexrecorder.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "GLEW_STATIC"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/linuxBuild/3rd_party/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/afs/athena.mit.edu/user/j/o/johnla/Documents/6.837/smoke_and_mirrors/linuxBuild/vecmath/CMakeFiles/vecmath.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../vecmath/include"
+  "../3rd_party/glfw/include"
+  "../3rd_party/glew/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ linuxBuild/CMakeFiles/simulation.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/simulation.dir/3rd_party/glew/src/glew.c.o"
+  "CMakeFiles/simulation.dir/src/main.cpp.o"
+  "CMakeFiles/simulation.dir/src/starter3_util.cpp.o"
+  "CMakeFiles/simulation.dir/src/camera.cpp.o"
+  "CMakeFiles/simulation.dir/src/vertexrecorder.cpp.o"
+  "CMakeFiles/simulation.dir/src/clothsystem.cpp.o"
+  "CMakeFiles/simulation.dir/src/timestepper.cpp.o"
+  "CMakeFiles/simulation.dir/src/particlesystem.cpp.o"
+  "CMakeFiles/simulation.dir/src/pendulumsystem.cpp.o"
+  "CMakeFiles/simulation.dir/src/rigidBall.cpp.o"
+  "CMakeFiles/simulation.dir/src/mirror.cpp.o"
+  "simulation.pdb"
+  "simulation"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C CXX)
+  INCLUDE(CMakeFiles/simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
